92cdbe827246174b1dc5a4f5e4b8466e87af3117
Your Name <malikkhan958957@gmail.com>
2022-11-04 00:58:01 +0530

    code fixed for prime problem

--- prime.js
+++ prime.js
@@ -1,8 +1,20 @@
-let num = 13 ;
-for (i=1; i<=num.length-1; i++){
-    if (num[i]%2==0){
-        console.log("It's a Prime number ");
-    } else {
-        console.log("it's not a prime number")
+function checkPrime(number){
+    let factor=0;
+    
+    for(let i=1;i<=number;i++){
+    if(number%i==0){
+    factor++;
     }
-}
+    }
+    if (count ==2 ){
+    return ture;
+    
+    }
+    return false;
+    }
+    let answer = checkPrime(13)
+    if(answer==ture){
+    console.log("Prime Number")
+    }else{
+    console.log("Not a Prime Number")
+    }
