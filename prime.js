4e31f4814a9b14541d76dcfda517d31cc56dba9b
Your Name <malikkhan958957@gmail.com>
2022-11-03 23:54:04 +0530

    Prime problem files

---
+++ prime.js
@@ -0,0 +1,8 @@
+let num = 13 ;
+for (i=1; i<=num.length-1; i++){
+    if (num[i]%2==0){
+        console.log("It's a Prime number ");
+    } else {
+        console.log("it's not a prime number")
+    }
+}
